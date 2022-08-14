fcced3a2269e03845e05d0e22b96379477b500bf
KetchuppOfficial <dolgov04@list.ru>
2022-08-14 16:13:18 +0300

    Tools.h are included

--- Laba_Helper/src/main.c
+++ Laba_Helper/src/main.c
@@ -1,8 +1,16 @@
+#include "Tools.h"
+
 int User_Interface (void);
 
 int main (void)
 {
-    User_Interface ();
+    if (User_Interface () == error)
+    {
+        #ifdef LABA_HELPER_DEBUG
+        printf ("User_Interface () terminated with error\n");
+        #endif // LABA_HELPER_DEBUG
+        return 1;
+    }
 
     return 0;
 }
